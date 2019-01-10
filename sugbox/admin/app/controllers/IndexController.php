030842ae4283b7803d2d9d52cc0ca51c43d57f4d
eddraven <eduardohr91@hotmail.com>
2019-01-10 11:23:13 -0500

    commit 6 eduardo: actalizando archivo IndexController.php en sugbox/admin/app/controllers

--- sugbox/admin/app/controllers/IndexController.php
+++ sugbox/admin/app/controllers/IndexController.php
@@ -8,6 +8,11 @@ class IndexController {
 		View::render($this,"index/index",array("meta"=>$meta));
 	}
 
+	public function unitsAction(){
+		$meta = array("title"=>".: HOSPITAL :.");
+		View::render($this,"index/units",array("meta"=>$meta));
+	}
+
 	
 
 }
