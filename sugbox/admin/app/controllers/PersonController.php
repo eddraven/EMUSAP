4a078fb511741731d4a87984c64cee443275d5fb
eddraven <eduardohr91@hotmail.com>
2019-01-11 10:58:38 -0500

    commit 12 eduardo: actualizando archivo PersonController.php a sugbox/admin/app/controllers version 3

--- sugbox/admin/app/controllers/PersonController.php
+++ sugbox/admin/app/controllers/PersonController.php
@@ -39,6 +39,27 @@ class PersonController {
 			));
 	Core::redir("./?r=team/open&id=$_POST[sucursal_id]");
 	}
+	
+	public function updateAction(){
+		$criteria = new Criteria("person");
+		$person=PersonData::getById($_POST["id"]);
+
+		$criteria->update(array(
+			"no"=>"\"$_POST[no]\"",
+			"name"=>"\"$_POST[name]\"",
+			"lastname"=>"\"$_POST[lastname]\"",
+			"job"=>"\"$_POST[job]\"",
+			"phone1"=>"\"$_POST[phone1]\"",
+			"phone2"=>"\"$_POST[phone2]\"",
+			"address1"=>"\"$_POST[address1]\"",
+			"address2"=>"\"$_POST[address2]\"",
+			"email1"=>"\"$_POST[email1]\"",
+			"email2"=>"\"$_POST[email2]\"",
+			"team_id"=>"\"$_POST[team_id]\"",
+			"category_id"=>"\"$_POST[category_id]\""
+			),"id=".$_POST["id"]);
+		Core::redir("./?r=index/people");
+	}	
 }
 
 
